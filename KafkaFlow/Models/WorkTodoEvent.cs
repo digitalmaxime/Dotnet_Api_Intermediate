ad1b9e9e7095e3096426f9b5649bf4f150572027
digitalmaxime <maxime.rheault@gmail.com>
2025-06-18 09:24:28 -0400

    backward compatible with default value for properties

--- KafkaFlow/Models/WorkTodoEvent.cs
+++ KafkaFlow/Models/WorkTodoEvent.cs
@@ -1,11 +1,17 @@
-using System;
-using System.Collections.Generic;
-using Avro;
-using Avro.Specific;
+using System.ComponentModel;
+
 
 namespace Models;
 
 public class WorkTodoEvent
 {
     public required string Description { get; set; }
+
+    public string? Note { get; set; } 
+    
+    [DefaultValue("")]
+    public string? Note2 { get; set; }
+    
+    [DefaultValue("")]
+    public string? Note3 { get; set; } 
 }
